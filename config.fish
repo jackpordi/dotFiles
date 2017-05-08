d74eb787d7741651448ba2d6a3c52bd4c6ee23c5
pp2916 <pp2916@ic.ac.uk>
2017-05-08 16:24:05 +0100

    added fish

---
+++ config.fish
@@ -0,0 +1,15 @@
+
+test $TERM != "screen"; and exec tmux
+
+neofetch
+
+set -g theme_color_scheme solarized-light
+
+set fish_function_path $fish_function_path "/usr/lib/python3.6/site-packages/powerline/bindings/fish"
+powerline-setup
+
+
+
+#sl
+
+
