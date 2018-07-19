0959a42807912adb24ee715124c514df0b2ccb53
Jack Pordi <jackel119@gmail.com>
2018-07-19 22:24:11 +0100

    Added powerline daemon

--- config.fish
+++ config.fish
@@ -3,6 +3,7 @@ test $TERM != "screen"; and exec tmux
 
 export TERM=xterm-256color
 
+powerline-daemon -q
 set fish_function_path $fish_function_path "/usr/lib/python3.6/site-packages/powerline/bindings/fish"
 powerline-setup
 
