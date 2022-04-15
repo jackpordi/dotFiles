b3784e6b904a486a8db9de6bb9a403601c67312e
Jack Pordi <jackel119@gmail.com>
2022-04-15 17:20:09 +0100

    Update cmp

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -29,7 +29,7 @@ cmp.setup {
   },
   map_cr = true,
   map_complete = true,
-  mapping = {
+  mapping = cmp.mapping.preset.insert {
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
