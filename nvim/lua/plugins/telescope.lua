f8417528969ec269440f9d8f06650a02059ff2e2
Jack Pordi <jackel119@gmail.com>
2022-08-11 10:10:35 +0700

    Add symbol search with telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,10 +1,15 @@
-require("telescope").load_extension 'fzf'
+local telescope = require("telescope")
+
+telescope.load_extension 'fzf'
 local remap = require("../remap")
+
+telescope.setup{ defaults = { file_ignore_patterns = {"node_modules"} } }
 
 remap("n", "<Leader>b", ":Telescope buffers<CR>")
 remap("n", "<C-p>", ":Telescope find_files find_command=rg,--follow,--files<CR>")
 
 remap("n", "<Leader>fw", ":Telescope live_grep<CR>")
+remap("n", "<Leader>s", ":Telescope lsp_dynamic_workspace_symbols<CR>")
 remap("n", "<Leader>gt", ":Telescope git_status<CR>")
 remap("n", "<Leader>cm", ":Telescope git_commits<CR>")
 remap("n", "<Leader>fd", ":Telescope find_directories<CR>")
