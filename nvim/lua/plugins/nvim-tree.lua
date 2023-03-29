9e9ce0b1648ef75d059aacaa6205033fbae7f24b
Jack Pordi <jackel119@gmail.com>
2023-03-29 22:10:03 +0100

    wip

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -9,5 +9,7 @@ ntree.setup {
 }
 local remap = require("../remap")
 
-remap.lua("n", "<Leader>nt", function() ntree.toggle(false, false) end)
-remap.lua("n", "<Leader>ff", function() ntree.toggle(true, false) end)
+local ntree_api = require("nvim-tree.api");
+
+remap.lua("n", "<Leader>nt", function() ntree_api.tree.toggle(false, false) end)
+remap.lua("n", "<Leader>ff", function() ntree_api.tree.toggle(true, false) end)
